f277a0caf047cba7117e364a8d372b31b6728b66
ninaveigaa <nvlmds@kth.se>
2026-07-13 16:05:05 +0200

    testing for 50k iterations

--- jobs/Wang_2023_train.sh
+++ jobs/Wang_2023_train.sh
@@ -2,7 +2,7 @@
 #SBATCH -A NAISS2025-3-39 -p alvis
 #SBATCH -N 1
 #SBATCH --gpus-per-node=A100:1
-#SBATCH -t 1-00:00:00
+#SBATCH -t 03:00:00
 #SBATCH -J Wang_2023_train
 #SBATCH -o /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/logs/Wang_2023_train_%j.out
 #SBATCH -e /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/logs/Wang_2023_train_%j.err
@@ -16,20 +16,20 @@ source super-resolution/bin/activate
 mkdir -p logs metrics
 
 python pipelines/Wang_2023_runDualSRNetSlimCoupled.py \
-    --epoch 500 \
-    --itersPerEpoch 300 \
+    --epoch 50 \
+    --itersPerEpoch 100 \
     --iterCyclesPerEpoch 3 \
     --batch_size 16 \
     --fine_size 48 \
     --lr 1e-4 \
-    --epoch_step 50 \
+    --epoch_step 150 \
     --scale 4 \
     --ngsrf 64 \
     --numResBlocks 16 \
     --ganFlag False \
     --save_freq 10 \
     --print_freq 10 \
-    --valNum 5 \
+    --valNum 20 \
     --gpuIDs 0 \
     --modelName Wang_2023 \
     --dataset_dir /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/data/processed/Wang_2023/training/ \
