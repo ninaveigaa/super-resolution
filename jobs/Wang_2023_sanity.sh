1d0ae0f495afe0bdef28c78fa016c056d7ed0798
Nina Veiga Lemos Marins de Souza <marins@alvis1.int.private>
2026-07-13 16:53:30 +0200

    50k iterations training

--- jobs/Wang_2023_sanity.sh
+++ jobs/Wang_2023_sanity.sh
@@ -1,19 +1,35 @@
 #!/usr/bin/env bash
-# jobs/Wang_2023_sanity.sh
+#SBATCH -A NAISS2025-3-39 -p alvis
+#SBATCH -N 1
+#SBATCH --gpus-per-node=A100:1
+#SBATCH -t 1-00:00:00
+#SBATCH -J Wang_2023_sanity
+#SBATCH -o /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/logs/Wang_2023_sanity_%j.out
+#SBATCH -e /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/logs/Wang_2023_sanity_%j.err
 
+module purge
 module load TensorFlow/2.15.1-foss-2023a-CUDA-12.1.1 virtualenv/20.23.1-GCCcore-12.3.0
+
+cd /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/
 source super-resolution/bin/activate
 
-ARGS=(
-    --phase train
-    --epoch 1
-    --itersPerEpoch 2
-    --iterCyclesPerEpoch 1
-    --print_freq 1
-    --valNum 1
-    --fine_size 16
-    --dataset_dir "data/processed/Wang_2023/training/"
-    --modelName Wang_2023_sanity
-)
-
-python pipelines/Wang_2023_runDualSRNetSlimCoupled.py "${ARGS[@]}"
+python pipelines/Wang_2023_runDualSRNetSlimCoupled.py \
+    --epoch 1 \
+    --itersPerEpoch 1 \
+    --iterCyclesPerEpoch 1 \
+    --batch_size 16 \
+    --fine_size 48 \
+    --lr 1e-4 \
+    --epoch_step 50 \
+    --scale 4 \
+    --ngsrf 64 \
+    --numResBlocks 16 \
+    --ganFlag False \
+    --save_freq 10 \
+    --print_freq 10 \
+    --valNum 1 \
+    --gpuIDs 0 \
+    --modelName Wang_2023 \
+    --dataset_dir /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/data/processed/Wang_2023/training/ \
+    --val_dataset_dir /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/data/processed/Wang_2023/validation/ \
+    --checkpoint_dir /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/checkpoints
