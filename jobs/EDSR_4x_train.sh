7c2c75c3e752be86ba7224f0117a23168e714347
ninaveigaa <nvlmds@kth.se>
2026-07-19 20:28:54 +0200

    .

--- jobs/EDSR_4x_train.sh
+++ jobs/EDSR_4x_train.sh
@@ -4,18 +4,18 @@
 #SBATCH --gpus-per-node=A100:1
 #SBATCH -t 20:00:00
 #SBATCH -J EDSR_x4
-#SBATCH -o /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/logs/edsr_x4_%j.out
-#SBATCH -e /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/logs/edsr_x4_%j.err
+#SBATCH -o logs/edsr_x4_%j.out
+#SBATCH -e logs/edsr_x4_%j.err
 
 set -euo pipefail
 
-source /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/envs/super-resolution-pytorch/bin/activate
+source envs/super-resolution-pytorch/bin/activate
 
-cd /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/submodules/EDSR-PyTorch/src
+cd submodules/EDSR-PyTorch/src
 
 python main.py \
     --template EDSR_paper \
     --scale 4 \
     --save edsr_x4_scratch \
-    --dir_data /mimer/NOBACKUP/groups/kthmech/nvlmds/super-resolution/dataset \
+    --dir_data ../../../data/raw \
     --reset
